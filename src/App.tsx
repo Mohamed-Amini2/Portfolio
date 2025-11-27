a3536931d224383b4392c5a2258fdce6bdac7352
Mohamed-Amini2 <Mohamedamini740@gmail.com>
2025-11-27 18:18:47 +0700

    fixing router basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import {  BrowserRouter, Route, Routes, } from 'react-router-dom';
 import { Portfolio } from './pages/Portfolio';
 import './main.css';
@@ -6,11 +5,11 @@ import './main.css';
 function App() {
 
   return (
-    <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<Portfolio />}/>
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter basename="/Portfolio">
+      <Routes>
+          <Route path='/' element={<Portfolio />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
